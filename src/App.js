75e8b3fc171ba73fa320f25cee8220811af52855
ljandora7 <ltj00002@mix.wvu.edu>
2024-03-05 18:31:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1>Cs 230L</h1>
+          <h2>Section - 02</h2>
+          <p>WVU ID: 800427779</p>
+          <p>Hi I am Luke</p>
+    </div>
+  );
+}
+
+export default App;
